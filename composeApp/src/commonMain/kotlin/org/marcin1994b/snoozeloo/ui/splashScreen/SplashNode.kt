530083026e1802cc623094746ab3af97ed02af39
marcin baranski <marcin.baranski@inspeerity.com>
2024-11-12 15:08:03 +0100

    Created Splash Screen.

--- composeApp/src/commonMain/kotlin/org/marcin1994b/snoozeloo/ui/splashScreen/SplashNode.kt
+++ composeApp/src/commonMain/kotlin/org/marcin1994b/snoozeloo/ui/splashScreen/SplashNode.kt
@@ -1,26 +1,31 @@
 package org.marcin1994b.snoozeloo.ui.splashScreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.bumble.appyx.navigation.modality.NodeContext
 import com.bumble.appyx.navigation.node.LeafNode
-import org.marcin1994b.snoozeloo.theme.AppTheme
+import org.jetbrains.compose.resources.painterResource
+import org.marcin1994b.snoozeloo.theme.AppColors
+import snoozeloo.composeapp.generated.resources.Res
+import snoozeloo.composeapp.generated.resources.alarm_icon
 
 class SplashNode(nodeContext: NodeContext) : LeafNode(nodeContext) {
 
     @Composable
     override fun Content(modifier: Modifier) {
         Box(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize()
+                .background(color = AppColors.BrandBlue),
             contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = "Splash Screen View",
-                color = AppTheme.colorScheme.error
+            Image(
+                painter = painterResource(Res.drawable.alarm_icon),
+                contentDescription = null
             )
         }
     }
