0abdf7c88fb521a0cf8580e58b7f8d9c8277c41f
marcin baranski <marcin.baranski@inspeerity.com>
2024-11-19 14:54:32 +0100

    Created Set Alarm View

---
+++ composeApp/src/commonMain/kotlin/org/marcin1994b/snoozeloo/ui/alarmScreen/AlarmListLoadedStateView.kt
@@ -0,0 +1,53 @@
+package org.marcin1994b.snoozeloo.ui.alarmScreen
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.marcin1994b.snoozeloo.design.AlarmCardView
+import org.marcin1994b.snoozeloo.model.Alarm
+import org.marcin1994b.snoozeloo.theme.AppTheme
+
+@Composable
+fun AlarmListLoadedStateView(items: List<Alarm>) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp)
+    ) {
+
+        item {
+            Spacer(Modifier.height(16.dp))
+        }
+
+        item {
+            Text(
+                text = "Your Alarms",
+                style = AppTheme.typography.headline5
+            )
+        }
+
+        item {
+            Spacer(Modifier.height(16.dp))
+        }
+
+        items.forEach { alarmData ->
+            item {
+//                AlarmCardView(
+//                    title = alarmData.name,
+//                    timestamp = alarmData.time,
+//                    isTurnOn = alarmData.isOn,
+//                    repeatOn = alarmData.repeatOn,
+//                    onSwitchClick = {}
+//                )
+            }
+
+            item {
+                Spacer(Modifier.height(16.dp))
+            }
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/org/marcin1994b/snoozeloo/ui/alarmScreen/AlarmListNode.kt
@@ -0,0 +1,45 @@
+package org.marcin1994b.snoozeloo.ui.alarmScreen
+
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.bumble.appyx.navigation.modality.NodeContext
+import com.bumble.appyx.navigation.node.LeafNode
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+import org.kodein.di.instance
+import org.marcin1994b.snoozeloo.design.AppFloatingActionButton
+import org.marcin1994b.snoozeloo.theme.AppColors
+
+class AlarmListNode(
+    nodeContext: NodeContext,
+    override val di: DI
+) : LeafNode(nodeContext), DIAware {
+
+    private val viewModel: AlarmListViewModel by instance()
+
+    @Composable
+    override fun Content(modifier: Modifier) {
+        val scaffoldState: ScaffoldState = rememberScaffoldState()
+
+        val viewState = viewModel.alarmListData.value
+
+        Scaffold(
+            scaffoldState = scaffoldState,
+            backgroundColor = AppColors.Grey075,
+            floatingActionButton = {
+                AppFloatingActionButton(
+                    onClick = {}
+                )
+            }
+        ) {
+            when (viewState) {
+                AlarmListViewState.Loading -> {}
+                AlarmListViewState.Empty -> AlarmListEmptyStateView()
+                is AlarmListViewState.Loaded -> AlarmListLoadedStateView(viewState.alarms)
+            }
+        }
+    }
+}
